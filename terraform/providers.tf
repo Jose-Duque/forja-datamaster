ee4b2f91472f2a2f562b1d10d423bae4b4619dd4
jose duque <cj_duque@hotmail.com>
2025-07-24 20:00:24 -0300

    inicializacao do terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.0.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">= 1.30.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
